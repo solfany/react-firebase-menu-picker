3e1e3f0b8359352cc945159a64ebca0770da1063
DESKTOP-9K23CHS\np <solfany1999@gmail.com>
2025-04-15 08:38:12 +0900

    chore: url root 경로 삽입

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import "./styles/globals.scss";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+    <BrowserRouter basename="/react-firebase-menu-picker">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
