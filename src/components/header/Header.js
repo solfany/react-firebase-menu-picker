7e32c2b5b0581cb58e0fac311824dde49b75ddad
DESKTOP-9K23CHS\np <solfany1999@gmail.com>
2025-05-13 10:07:58 +0900

    design: 로고 및 컬러 변경

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -50,7 +50,7 @@ const Header = () => {
     >
       <div className="lunch-header__container">
         <h1 className="lunch-header__title">
-          <span className="lunch-header__title-text">엔피로지스</span>
+          <span className="lunch-header__title-text"><span className="lunch-header__title-text-primary">NP</span>icker</span>
           <span className="lunch-header__title-highlight">점심메뉴 서비스</span>
         </h1>
 
