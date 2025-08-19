bc26bc7f266a573dc8050fec5c0bc3169dcb48db
solfany <solfany1999@gmail.com>
2025-08-19 10:58:34 +0900

    set: delete sample data

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,14 +6,7 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAgGahz1MYbA2ZrdHKGSFmtZsdjea-ILMg",
-  authDomain: "np-menu-60116.firebaseapp.com",
-  databaseURL:
-    "https://np-menu-60116-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "np-menu-60116",
-  storageBucket: "np-menu-60116.firebasestorage.app",
-  messagingSenderId: "671133084387",
-  appId: "1:671133084387:web:60d9034a49ab1bc937f3d6",
+
 };
 
 // Initialize Firebase
